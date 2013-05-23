24e79f55a4dce49b07f201422c669ec47d43b19f
Michal Pasternak <michal.dtz@gmail.com>
2013-05-23 09:43:46 +0200

    Support situation, when creatinine in urine is in a different sample, than in plasma

--- clearance.py
+++ clearance.py
@@ -11,19 +11,24 @@ CREATININE_IN_URINE = 'Kreatynina w moczu'
 c = cobas.Server()
 c.login()
 
+plasma = urine = None
+
 for sample in c.find_by_name(sys.argv[1]):
     badanie = c.get_single_result(sample['url'])
 
-    if CREATININE in badanie.keys() and CREATININE_IN_URINE in badanie.keys():
+    if CREATININE in badanie.keys() and plasma is None:
+        plasma = float(badanie[CREATININE]['value'])
+
+    if CREATININE_IN_URINE in badanie.keys() and urine is None:
+        urine = float(badanie[CREATININE_IN_URINE]['value'])
+
+    if urine is not None and plasma is not None:
         ile_moczu = float(raw_input('Amount of urine (ml)> '))
 
         try:
             hours = float(raw_input('Amount of hours (default=24)> '))
         except ValueError:
             hours = 24
-
-        plasma = float(badanie[CREATININE]['value'])
-        urine = float(badanie[CREATININE_IN_URINE]['value'])
 
         print "Clearance: %.2f ml/min" % (urine*ile_moczu/(plasma*hours*60))
         break
